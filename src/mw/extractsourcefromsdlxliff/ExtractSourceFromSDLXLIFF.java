974eab69714705146b91027457f3eae72feb8150
Mark Wiltshire <mark.wiltshire88@gmail.com>
2018-11-16 21:41:15 +0100

    Add listAllFiles method

--- src/mw/extractsourcefromsdlxliff/ExtractSourceFromSDLXLIFF.java
+++ src/mw/extractsourcefromsdlxliff/ExtractSourceFromSDLXLIFF.java
@@ -1,10 +1,32 @@
 package mw.extractsourcefromsdlxliff;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExtractSourceFromSDLXLIFF {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		listAllFiles(Paths.get("./"), "*.sdlxliff");
+		
+	}
+	
+	public static List<Path> listAllFiles(Path directory, String acceptedFileNames) {
+		List<Path> files = new ArrayList<>();
+		
+		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(directory, acceptedFileNames)) {
+			for (Path file : directoryStream) {
+				files.add(file);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return files;
 	}
 
 }
